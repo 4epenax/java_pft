c5077a1197f27e3b1d56fa2f25628d61fcbea9d2
waka <a.cherepahin@id-sys.com>
2018-08-09 13:07:16 +0300

    Point class changed

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -11,9 +11,15 @@ public class MyFirstProgram {
 
         Rectangle r = new Rectangle(4, 6);
         System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
+
+        Point p1 = new Point(2, 4);
+        Point p2 = new Point(6, 8);
+        System.out.println("Расстояние между точками равно: " + p1.distance(p2));
     }
 
     public static void hello(String somebody) {
         System.out.println("Hello, " + somebody + "!");
     }
+
 }
