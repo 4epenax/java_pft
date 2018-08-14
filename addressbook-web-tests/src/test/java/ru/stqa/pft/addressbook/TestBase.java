741ff0998564adadec91cdf39ce3a9a0baa68a1b
waka <x0bo@ya.ru>
2018-08-14 22:23:31 +0300

    moving class [ApplicationManager]
    класс ApplicationManager перемещён в пакет appmanager

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/TestBase.java
@@ -2,6 +2,7 @@ package ru.stqa.pft.addressbook;
 
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
