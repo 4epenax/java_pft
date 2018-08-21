4401aaa96bd58a913de6161bb1980fadeb89a2a9
waka <a.cherepahin@id-sys.com>
2018-08-21 10:43:28 +0300

    code optimization [contacts class]
    добавлен метод group для контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -8,7 +8,7 @@ public class ContactCreationTests extends TestBase {
     @Test
     public void testContactCreation() {
         app.getContactHelper().initContactCreation();
-        app.getContactHelper().fillContactForm(new ContactData("testFname", "testLname", "224466", "test@mail.tt"));
+        app.getContactHelper().fillContactForm(new ContactData("testFname", "testLname", "224466", "test@mail.tt", "test3"));
         app.getContactHelper().submitContactCreation();
         app.getNavigationHelper().gotoHomePage();
     }
