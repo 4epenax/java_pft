d6928e3b1c8fda246a318cc4f9e81617db7fde4a
4epenax <x0bo@ya.ru>
2018-09-09 01:17:21 +0300

    добавлены сеттеры

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -2,9 +2,10 @@ package ru.stqa.pft.addressbook.model;
 
 public class GroupData {
     private int id;
-    private final String name;
-    private final String header;
-    private final String footer;
+    private String name;
+    private String header;
+
+    private String footer;
 
     public GroupData(String name, String header, String footer) {
         this.id = Integer.MAX_VALUE;
@@ -24,8 +25,24 @@ public class GroupData {
         return id;
     }
 
-    public void setId(int id) {
+    public GroupData withId(int id) {
         this.id = id;
+        return this;
+    }
+
+    public GroupData withName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public GroupData withHeader(String header) {
+        this.header = header;
+        return this;
+    }
+
+    public GroupData withFooter(String footer) {
+        this.footer = footer;
+        return this;
     }
 
     public String getName() {
