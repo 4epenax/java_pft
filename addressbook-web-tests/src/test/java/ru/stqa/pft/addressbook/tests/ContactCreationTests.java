9ed9e8e82dbc768e5d50b348ffe5b947bd7c690f
waka <a.cherepahin@id-sys.com>
2018-10-08 18:01:38 +0300

    чтение данных из файла .csv для создания контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
-import java.io.File;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -16,15 +16,17 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class ContactCreationTests extends TestBase {
 
     @DataProvider
-    public Iterator<Object[]> validContacts() {
+    public Iterator<Object[]> validContacts() throws IOException {
         List<Object[]> list = new ArrayList<Object[]>();
-        File photo = new File("src/test/resources/usertile30.jpg");
-        list.add(new Object[]{new ContactData().withFirstname("First").withLastname("Last").withAddress("Russia")
-                .withEmail("test@mail.tt").withEmail2("test2@mail.tt").withEmail3("test3@mail.tt")
-                .withHomePhone("111").withMobilePhone("222").withWorkPhone("333").withPhoto(photo).withGroup("test2")});
-        list.add(new Object[]{new ContactData().withFirstname("First0").withLastname("Last0").withAddress("Russia0")
-                .withEmail("test0@mail.tt").withEmail2("test02@mail.tt").withEmail3("test03@mail.tt")
-                .withHomePhone("1110").withMobilePhone("2220").withWorkPhone("3330").withPhoto(photo).withGroup("test2")});
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.csv")));
+        String line = reader.readLine();
+        while (line != null) {
+            String[] split = line.split(";");
+            list.add(new Object[] {new ContactData().withFirstname(split[0]).withLastname(split[1]).withAddress(split[2])
+                    .withEmail(split[3]).withEmail2(split[4]).withEmail3(split[5]).withHomePhone(split[6])
+                    .withMobilePhone(split[7]).withWorkPhone(split[8]).withGroup(split[9]).withPhoto(new File(split[10]))});
+            line = reader.readLine();
+        }
         return list.iterator();
     }
 
