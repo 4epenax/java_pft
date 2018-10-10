ab92ab2d7fd210cf6ad7bcb74a7f0b02940fa70c
waka <a.cherepahin@id-sys.com>
2018-10-10 13:23:07 +0300

    заточены заготовки для новых тестов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddContactToGroupTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddContactToGroupTests.java
@@ -1,4 +1,17 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 public class AddContactToGroupTests {
+
+    @BeforeMethod
+    public void ensurePreconditions() {
+
+    }
+
+    @Test
+    public void testAddContactToGroup() {
+
+    }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/RemoveContactFromGroupTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/RemoveContactFromGroupTests.java
@@ -1,4 +1,17 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 public class RemoveContactFromGroupTests {
+
+    @BeforeMethod
+    public void ensurePreconditions() {
+
+    }
+
+    @Test
+    public void testRemoveContactFromGroup() {
+
+    }
 }
