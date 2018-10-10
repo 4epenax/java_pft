6340335db91be535b791a1df2e4246ebba783353
waka <a.cherepahin@id-sys.com>
2018-10-10 13:25:50 +0300

    заготовки еще острее

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/RemoveContactFromGroupTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/RemoveContactFromGroupTests.java
@@ -2,12 +2,16 @@ package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
 
-public class RemoveContactFromGroupTests {
+public class RemoveContactFromGroupTests extends TestBase {
 
     @BeforeMethod
     public void ensurePreconditions() {
-
+        if (app.db().groups().size() == 0) {
+            app.goTo().groupPage();
+            app.group().create(new GroupData().withName("test1"));
+        }
     }
 
     @Test
