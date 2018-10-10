24679f14d942ab6ce7d2746f9fad9190b5735717
waka <a.cherepahin@id-sys.com>
2018-10-10 13:36:32 +0300

    добавлены предусловия к тестам контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactAddressTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactAddressTests.java
@@ -3,6 +3,7 @@ package ru.stqa.pft.addressbook.tests;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
 
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -12,6 +13,10 @@ public class ContactAddressTests extends TestBase {
 
     @BeforeMethod
     public void ensurePreconditions() {
+        if (app.db().groups().size() == 0) {
+            app.goTo().groupPage();
+            app.group().create(new GroupData().withName("test1"));
+        }
         if (app.db().contacts().size() == 0) {
             Groups groups = app.db().groups();
             app.goTo().homePage();
