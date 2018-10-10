12ebde6a82ee6e5c7d54890ac47386d9203a6e80
4epenax <x0bo@ya.ru>
2018-10-10 20:19:25 +0300

    еще предусловия

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddContactToGroupTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddContactToGroupTests.java
@@ -2,7 +2,9 @@ package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.Groups;
 
 public class AddContactToGroupTests extends TestBase {
 
@@ -11,6 +13,14 @@ public class AddContactToGroupTests extends TestBase {
         if (app.db().groups().size() == 0) {
             app.goTo().groupPage();
             app.group().create(new GroupData().withName("test1"));
+        }
+        if (app.db().contacts().size() == 0) {
+            Groups groups = app.db().groups();
+            app.goTo().homePage();
+            app.contact().create(new ContactData()
+                    .withFirstname("First").withLastname("Last").withAddress("Russia")
+                    .withEmail("test@mail.tt").withEmail2("test@mail2.tt").withEmail3("test3@mail.tt")
+                    .withHomePhone("111").withMobilePhone("222").withWorkPhone("333").inGroup(groups.iterator().next()));
         }
     }
 
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/RemoveContactFromGroupTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/RemoveContactFromGroupTests.java
@@ -2,7 +2,9 @@ package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.Groups;
 
 public class RemoveContactFromGroupTests extends TestBase {
 
@@ -12,6 +14,15 @@ public class RemoveContactFromGroupTests extends TestBase {
             app.goTo().groupPage();
             app.group().create(new GroupData().withName("test1"));
         }
+        if (app.db().contacts().size() == 0) {
+            Groups groups = app.db().groups();
+            app.goTo().homePage();
+            app.contact().create(new ContactData()
+                    .withFirstname("First").withLastname("Last").withAddress("Russia")
+                    .withEmail("test@mail.tt").withEmail2("test@mail2.tt").withEmail3("test3@mail.tt")
+                    .withHomePhone("111").withMobilePhone("222").withWorkPhone("333").inGroup(groups.iterator().next()));
+        }
+
     }
 
     @Test
