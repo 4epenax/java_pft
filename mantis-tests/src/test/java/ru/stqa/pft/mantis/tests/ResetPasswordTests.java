ac63ae643b4e3ed6313f0c577937cd5d24e581df
waka <x0bo@ya.ru>
2018-10-15 05:01:57 +0300

    ResetPasswordTests working

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/ResetPasswordTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/ResetPasswordTests.java
@@ -1,5 +1,7 @@
 package ru.stqa.pft.mantis.tests;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.lanwen.verbalregex.VerbalExpression;
 import ru.stqa.pft.mantis.model.MailMessage;
@@ -12,7 +14,7 @@ import static org.testng.Assert.assertTrue;
 
 public class ResetPasswordTests extends TestBase {
 
-    //@BeforeMethod
+    @BeforeMethod
     public void startMailServer() {
         app.mail().start();
     }
@@ -37,7 +39,7 @@ public class ResetPasswordTests extends TestBase {
         return regex.getText(mailMessage.text);
     }
 
-    //@AfterMethod
+    @AfterMethod
     public void stopMailServer() {
         app.mail().stop();
     }
