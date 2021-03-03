b5f3726b2fb837727cb661783848fb0598f1bbb6
w4k4 <a4epenax@gmail.com>
2021-03-03 23:00:00 +0300

    Dog.java + DogTestDrive.java

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Dog.java
@@ -0,0 +1,14 @@
+package ru.stqa.pft.sandbox;
+
+public class Dog {
+
+    // переменные экземпляра:
+    int size;
+    String breed;
+    String name;
+
+    // методы:
+    void bark(){
+        System.out.println("Гав! Гав!");
+    }
+}
