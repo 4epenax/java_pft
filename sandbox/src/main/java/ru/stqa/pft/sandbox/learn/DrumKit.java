fe7365d823ed7ad46a294f6a2afabf924634b8d8
w4k4 <a4epenax@gmail.com>
2021-03-05 22:48:01 +0300

    DrumKit.java + DrumKitTestDrive.java

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/learn/DrumKit.java
@@ -0,0 +1,16 @@
+package ru.stqa.pft.sandbox.learn;
+
+public class DrumKit {
+
+    boolean top = true;
+    boolean snare = true;
+
+    void playTopHat(){
+
+        System.out.println("динь динь ди-динь");
+    }
+
+    void playSnare(){
+        System.out.println("бах бах ба-бах");
+    }
+}
